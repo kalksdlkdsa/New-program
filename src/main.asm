56358020766c4fbe06e4573352001b71a4f48b7b
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-03-02 16:51:22 +0700

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,8 +1,13 @@
 org 0x7C00
 bits 16
 main:
+    ; setup data segment
+    mov ax, 0 ; can't write to es/ds directly
+    mov ds, ax
+    mov ss, ax
+    mov sp, 0x7C00
     hlt
 .halt:
     jmp .halt
-times 510-($-$$) db 0
+times 510-($-%%) db 0
 dw 0AA55h
